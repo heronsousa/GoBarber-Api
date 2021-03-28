78bb54c2fb1d864bc4684dc621070a3ea7c01028
Heron Rodrigues <heronrs1@gmail.com>
2021-03-28 11:07:04 -0300

    Generate jwt

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -8,8 +9,13 @@ interface Request {
     password: string;
 }
 
+interface Response {
+    user: User;
+    token: string;
+}
+
 class AuthenticateUserService {
-    public async execute({ email, password }: Request): Promise<{ user: User }> {
+    public async execute({ email, password }: Request): Promise<Response> {
         const usersRepository = getRepository(User);
 
         const user = await usersRepository.findOne({ where: { email } });
@@ -24,7 +30,13 @@ class AuthenticateUserService {
             throw new Error('Email or password are incorrect');
         }
 
-        return { user };
+        const token = sign({}, '0d20c655e981ca60180b45235de75bbf', {
+            subject: user.id,
+            expiresIn: '1d',
+
+        });
+
+        return { user, token };
     }
 }
 
